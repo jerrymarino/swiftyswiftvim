01c2a076a96c354e40044692418ede40b594b77c
Jerry Marino <i@jerrymarino.com>
2017-04-16 19:30:53 -0700

    Remove cpp cruft

--- PythonExtensions.cpp
+++ PythonExtensions.cpp
@@ -1,42 +1,27 @@
-
 #include <boost/python.hpp>
+#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
 #include <string>
 #include "SwiftCompleter.h"
-#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
-
 
 using namespace boost::python;
 using namespace ssvim;
+
+// Wrap a std::vector<std::string> for Python Consumers
 typedef std::vector<std::string> StringList;
 
 struct Runner
 {
-    void set(std::string fileName,
-            std::string fileContents,
-            std::vector<std::string> flags,
-            unsigned line,
-            unsigned column) { 
-        this->fileName = fileName;
-        unsavedFile.contents = fileContents;
-        unsavedFile.fileName = fileName;
-        this->column = column;
-        this->line = line;
-        this->flags = flags;
-    }
-
-    std::string setAndRun(std::string fileName,
+    std::string complete(std::string fileName,
             std::string fileContents,
             std::vector<std::string> flags,
             unsigned line,
             unsigned column) {
-        set(fileName, fileContents, flags, line, column);
-        return run();
-    }
-
-    std::string run() { 
         auto completer = SwiftCompleter();        
-        auto flags = this->flags;
         auto files = std::vector<UnsavedFile>();
+        auto unsavedFile = UnsavedFile();
+        unsavedFile.contents = fileContents;
+        unsavedFile.fileName = fileName;
+
         files.push_back(unsavedFile);
         auto result = completer.CandidatesForLocationInFile(
                 fileName,
@@ -48,12 +33,6 @@ struct Runner
         return result;
     
     }
-
-    std::string fileName;
-    std::vector<std::string> flags;
-    UnsavedFile unsavedFile;
-    unsigned line;
-    unsigned column;
 };
 
 BOOST_PYTHON_MODULE(swiftvi)
@@ -61,8 +40,6 @@ BOOST_PYTHON_MODULE(swiftvi)
     class_<StringList>("StringList")
         .def(vector_indexing_suite<StringList>() );
     class_<Runner>("Runner")
-        .def("run", &Runner::run)
-        .def("complete", &Runner::setAndRun)
-        .def("setFile", &Runner::set)
+        .def("complete", &Runner::complete)
     ;
 };
--- SwiftCompleter.h
+++ SwiftCompleter.h
@@ -4,18 +4,30 @@
 
 namespace ssvim {
 
+/**
+ * An unsaved file.
+ * 
+ * In practice, this file is in the users vim buffer
+ * and changes have been made to it, but they have not
+ * been written to disk yet
+ */
 class UnsavedFile {
     public:
     std::string contents;
     std::string fileName;
 };
 
+/**
+ * Yield complitions in the form of json string.
+ *
+ * The completions match the API used within sourcekit
+ */
 class SwiftCompleter {
 public:
   SwiftCompleter();
   ~SwiftCompleter();
 
-  std::string  CandidatesForLocationInFile(
+  std::string CandidatesForLocationInFile(
     const std::string &filename,
     int line,
     int column,
