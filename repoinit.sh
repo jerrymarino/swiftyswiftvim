ed674fd9ce2aef4b05e661fefee1c3d126103fd0
Jerry Marino <i@jerrymarino.com>
2017-05-06 22:59:50 -0700

    Fix typo in repoinit.sh

--- repoinit.sh
+++ repoinit.sh
@@ -6,7 +6,7 @@ cd vendor/
 curl -Lo BeastDL https://github.com/vinniefalco/Beast/archive/6d5547a32c50ec95832c4779311502555ab0ee1f.zip
 unzip BeastDL
 mkdir -p beast
-ditto BeastDL/Beast-6d5547a32c50ec95832c4779311502555ab0ee1f/ beast
+ditto Beast-6d5547a32c50ec95832c4779311502555ab0ee1f/ beast/
 rm -rf BeastDL
-
+rm -rf Beast-6d5547a32c50ec95832c4779311502555ab0ee1f
 
