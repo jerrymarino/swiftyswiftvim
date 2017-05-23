dc78db125aa70054c726ca0033d3a2799c9c7a1f
Jerry Marino <i@jerrymarino.com>
2017-05-23 08:59:13 -0700

    Remove brew install boost
    This patch improves the clean build performance and predictability of
    the build by removing the previous `$brew pray and spray` build tactic
    for boost.
    
    Check in boost and build it with boost's build system via cmake. This is
    easier and uses boost's existing build abilities. Use boost-cp to
    intelligently fetch minimum set of dependent files. Then, further
    processing strips it down even more to minimize the repo footprint.
    
    Included is the script I used to achieve this with BCP, which needs some
    work to update again it again but is highly useful.

---
+++ build_boost.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Do a build of boost using the boost infra.
+# Libs go into build/boost/lib
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+SSVIM_BUILD_DIR=$DIR/build
+
+# Headers to /vendor/boost/include
+# Libs to /vendor/boost/lib
+BOOST_PREFIX=$SSVIM_BUILD_DIR/vendor/boost/
+BOOST_LIB_DIR=$SSVIM_BUILD_DIR/vendor/boost/lib/
+
+mkdir -p $BOOST_LIB_DIR
+
+# Wants to build from here.
+cd $DIR/vendor/boost
+./bootstrap.sh --prefix=$BOOST_PREFIX \
+    --libdir=$BOOST_LIB_DIR --without-icu
+
+./b2 --prefix=$BOOST_PREFIX \
+    --libdir=$BOOST_LIB_DIR \
+    --user-config=user-config.jam \
+    -d2 \
+    -j4 \
+    --layout-tagged install \
+    threading=multi \
+    link=static \
+    cxxflags=-stdlib=libc++ linkflags=-stdlib=libc++ 
+
